ed47ff701ab15d5ca5c71d3ea7972cb72a9c0d54
Luccas José Bezerra Fernandes <luccas.fernandes@sectec.local>
2025-09-03 17:05:04 -0300

    feat: added the docker compose and serilog

--- Program.cs
+++ Program.cs
@@ -1,12 +1,32 @@
 using MyTodo.Data;
+using Serilog;
 
-var builder = WebApplication.CreateBuilder(args);
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
 
-builder.Services.AddControllers();
-builder.Services.AddDbContext<AppDbContext>();
-var app = builder.Build();
+try
+{
+    var builder = WebApplication.CreateBuilder(args);
+
+    builder.Host.UseSerilog((context, config) => { config.ReadFrom.Configuration(context.Configuration); });
+
+    builder.Services.AddControllers();
+    builder.Services.AddDbContext<AppDbContext>();
+    var app = builder.Build();
+    app.UseSerilogRequestLogging();
 
 
-app.MapControllers();
+    app.MapControllers();
 
-app.Run();
+    Log.Information("Application started");
+    app.Run();
+}
+catch (Exception e)
+{
+    Log.Fatal(e, "Application terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
