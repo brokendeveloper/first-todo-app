7df964473f41ffd1cd19dd6255f1f2c426395708
Luccas José Bezerra Fernandes <luccas.fernandes@sectec.local>
2025-09-08 15:41:32 -0300

    feat: add the userContext and your middleware

---
+++ Services/Middleware/UserContextMiddleware.cs
@@ -0,0 +1,25 @@
+namespace MyTodo.Services.Middleware;
+
+public class UserContextMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public UserContextMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext context, UserContext userContext)
+    {
+        if (context.Request.Headers.TryGetValue("X-User-Id", out var userIdValue))
+        {
+            if (long.TryParse(userIdValue, out long userId))
+            {
+                
+                userContext.UserId = userId;
+            }
+        }
+        
+        await _next(context);
+    }
+}
