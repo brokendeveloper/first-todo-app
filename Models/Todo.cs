3ca766f8bc578a86400baf04bda2d8620587a119
Luccas José Bezerra Fernandes <luccas.fernandes@sectec.local>
2025-09-09 15:34:06 -0300

    refactor: change todo entity to new format

--- Models/Todo.cs
+++ Models/Todo.cs
@@ -1,9 +1,24 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace MyTodo.Models;
 
+[Table("todos")]
 public class Todo
 {
+    [Column("id")]
     public int  Id { get; set; }
+    
+    [Column("title")]
     public string Title { get; set; }
+   
+    [Column("done")]
     public bool Done { get; set; }
-    public DateTime Date { get; set; } = DateTime.Now;
+    
+    [Column("date")]
+    public DateTime Date { get; set; }
+    
+    [Column("user_id")]
+    public int UserId { get; set; }
+
+    public User User { get; set; } = null!;
 }
